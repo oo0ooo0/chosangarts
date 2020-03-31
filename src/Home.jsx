2468bc6a78cef18b3c115a0c0dedee0de07943de
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-31 21:37:03 +0900

    change css, Pamphlet img

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,12 +4,22 @@ import styled from 'styled-components';
 const StyledAbout = styled.main`
   margin-top: 200px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  h3 {
+    font-size: 2vw;
+    line-height: 1;
+    margin-bottom: 20px;
+    text-align: center;
+  }
   .imageWrap {
     text-align: center;
     width: 70%;
     /* background: rgba(0, 0, 0, 0.1); */
+
     img {
+      padding: 2px 0px;
+      border-bottom: 1px solid lightgray;
       width: 100%;
       :first-child {
         margin-bottom: 20px;
@@ -21,9 +31,15 @@ const StyledAbout = styled.main`
 function Home() {
   return (
     <StyledAbout>
+      <h3>
+        Chosang solo exhibition <br />
+        '대상성 없는 대상성'
+        <br />
+        Gallery MHK 2020.04.07~29
+      </h3>
       <div className='imageWrap'>
-        <img src='/img/objectA-1.jpg' />
-        <img src='/img/objectA-2.jpg' />
+        <img src='/img/objectA-1.jpeg' />
+        <img src='/img/objectA-2.jpeg' />
       </div>
     </StyledAbout>
   );
