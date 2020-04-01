1fb336d2687323dfcd26d5d7a312322b7cfb920f
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-01 18:47:53 +0900

    Update Contact.jsx
    remove form for sending maile
    cuz Client's request
    Add sns and address instead

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,68 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContact = styled.main`
-  .contactImg {
+  height: 65vh;
+  .container {
     border-top: 1px solid lightgray;
     border-bottom: 1px solid lightgray;
-    display: flex;
-    justify-content: center;
-    padding: 10px 0px;
-    margin-bottom: 10px;
-    img {
-      width: 50%;
-    }
-  }
-  .container {
     width: 100%;
     display: flex;
     justify-content: center;
-    height: 450px;
+    align-items: center;
+    margin-top: 30px;
+    padding: 10px 0px;
+    .contactImg {
+      display: flex;
+      width: 40%;
+      img {
+        width: 100%;
+      }
+    }
 
     .myform {
       display: flex;
       flex-direction: column;
-      width: 70%;
+      align-items: center;
+      width: 60%;
       padding: 14px;
-      label {
-        display: block;
-        font-weight: bold;
+      h4 {
+        font-size: 35px;
       }
-
-      input,
-      textarea {
-        border: solid 1px lightGray;
-        width: 100%;
-        margin: 2px 0 20px 0px;
-        font-size: 14px;
-      }
-
-      input {
-        height: 25px;
-        padding: 4px 10px;
-      }
-      textarea {
-        padding: 10px 10px;
-        overflow: auto;
-      }
-      button {
-        background: #333;
-        width: 100%;
-        padding: 5px 10px;
-        border-radius: 15px;
-        color: white;
-        font-size: 20px;
-        transition: all 0.5s ease;
-      }
-      button:hover {
-        background: #eee;
-        color: black;
+      address {
+        margin: 15px 0px;
       }
     }
   }
   @media (max-width: 768px) {
-    h2 {
-      border-bottom: 1px solid lightgray;
-    }
     .contactImg {
       display: none;
     }
@@ -77,35 +48,22 @@ function Contact() {
   return (
     <StyledContact>
       <h2 className='title'>Contact</h2>
-      <div className='contactImg'>
-        <img src='img/1006.jpeg'></img>
-      </div>
+
       <div className='container'>
+        <div className='contactImg'>
+          <img src='img/1006.jpeg' alt='컨택트 이미지'></img>
+        </div>
         <div className='myform'>
-          <form id='form' name='form' method='post' action='index.html'>
-            <label>Name</label>
-            <input type='text' name='name' placeholder='Name' id='name' />
-
-            <label>Email</label>
-            <input type='text' name='email' placeholder='Email' id='email' />
-
-            <label>Phone</label>
-            <input type='text' name='phone' placeholder='Phone' id='phone' />
-
-            <label>Web-site</label>
-            <input id='web' type='text' placeholder='Web-site' name='web' id='web' />
-
-            <label>Message</label>
-            <textarea
-              placeholder='Write something to us'
-              name='message'
-              id='message'
-              rows='5'
-            ></textarea>
-
-            <button type='submit'>Send</button>
-            <div className='spacer'></div>
-          </form>
+          <h4>Cho-sang</h4>
+          <p>Media Art & Painting & Installation</p>
+          <address>
+            경기도 안산시 단원구 고잔동 640번지
+            <br />
+            서울예술대학교 영상학부 디지털아트과
+          </address>
+          <div className='email'>
+            <a href='mail-to:cswart@never.com'>Email: cswart@never.com</a>
+          </div>
         </div>
       </div>
     </StyledContact>
